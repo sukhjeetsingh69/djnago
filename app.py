1be1ff0c93ce26beb52256bee0aeb1af1d6963d9
sukhjeetsingh69 <lolpkpol54@gamil.com>
2023-10-09 01:53:46 -0700

    new changes

---
+++ app.py
@@ -0,0 +1,48 @@
+from flask import Flask,render_template,request,redirect
+import mysql.connector
+conn=mysql.connector.connect(host="localhost",username="root",password="root",database="school")
+
+x=conn.cursor()
+
+
+
+app = Flask(__name__)
+
+
+
+
+@app.route("/")
+def home():
+    return render_template("base.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+    
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+    
+
+@app.route("/show")
+def show():
+    return render_template("show.html")
+
+@app.route("/saveinfo",methods=["POST"])    
+def saveinfo():
+    if request.method=="POST":
+        
+        tou=request.form.get("abc")
+        pop=request.form.get("dfg")
+
+        print(tou)
+        print(pop)
+        x.execute("insert into teacher(Title,Description) values(%s, %s)",(tou,pop))
+        conn.commit()
+        return redirect("/contact")
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
